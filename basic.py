95d36d8d705ac81ee4b5b5911e65093371f1264e
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2022-05-28 23:22:22 +0530

    Cropping an image

--- basic.py
+++ basic.py
@@ -1,4 +1,5 @@
 import cv2 as cv
+from cv2 import resize
 
 img = cv.imread('Photos/park.jpg')
 cv.imshow('Boston', img)
@@ -12,8 +13,23 @@ blur = cv.GaussianBlur(img, (7, 7), cv.BORDER_DEFAULT)
 cv.imshow('Blur', blur)
 
 # Edge Cascade
-
-canny = cv.Canny(img, 125, 175)
+canny = cv.Canny(blur, 125, 175)
 cv.imshow('Canny Edges', canny)
+
+# Dilating the image
+dilated = cv.dilate(canny, (7, 7), iterations=3)
+cv.imshow('Dilated', dilated)
+
+# Eroding
+eroded = cv.erode(dilated, (7, 7), iterations=3)
+cv.imshow('Eroded', eroded)
+
+# resize
+resized = cv.resize(img, (500, 500), interpolation=cv.INTER_AREA)
+cv.imshow('Resized', resized)
+
+# cropping
+croped = img[50:200, 200:400]
+cv.imshow('Cropped', croped)
 
 cv.waitKey(0)
