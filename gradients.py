4e6b9257e0a5d781c4c2ee2e56de271ed7b29853
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2022-06-03 22:29:34 +0530

    Edge Detection

--- gradients.py
+++ gradients.py
@@ -12,4 +12,15 @@ lap = cv.Laplacian(gray, cv.CV_64F)
 lap = np.uint8(np.absolute(lap))
 cv.imshow('Laplacian', lap)
 
+# Sobel
+sobelx = cv.Sobel(gray, cv.CV_64F, 1, 0)
+sobely = cv.Sobel(gray, cv.CV_64F, 0, 1)
+combine_sobel = cv.bitwise_or(sobelx, sobely)
+
+cv.imshow('Sobel X', sobelx)
+cv.imshow('Sobel Y', sobely)
+cv.imshow('Combine Sobel', combine_sobel)
+
+canny = cv.Canny(gray, 150, 175)
+cv.imshow('Canny', canny)
 cv.waitKey(0)
