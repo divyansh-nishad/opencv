bcd36acfd769eddf6e5610c21bb6fb37ada7eee5
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2022-06-03 22:24:19 +0530

    Gradient

---
+++ gradients.py
@@ -0,0 +1,15 @@
+import cv2 as cv
+import numpy as np
+
+img = cv.imread('Photos/park.jpg')
+cv.imshow('Park', img)
+
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('Gray', gray)
+
+# Laplacian
+lap = cv.Laplacian(gray, cv.CV_64F)
+lap = np.uint8(np.absolute(lap))
+cv.imshow('Laplacian', lap)
+
+cv.waitKey(0)
